68a0265928a1d49b05dc6b4c726d281d3af836bc
Richard Kats <richard.kats1@hm.edu>
2023-04-25 20:45:32 +0200

    added MapView and HM-Marker

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from "react-native-maps";
 
+const INITIAL_HM_LATITUDE = 48.15386371978465;
+const INITIAL_HM_LONGITUDE = 11.55259067796154;
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const initialRegion = {
+        latitude: INITIAL_HM_LATITUDE,
+        longitude: INITIAL_HM_LONGITUDE,
+        latitudeDelta: 0.008,
+        longitudeDelta: 0.008,
+    };
+
+    const hmCoordinates = {
+        latitude: 48.15386371978465,
+        longitude: 11.55259067796154,
+    };
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={initialRegion}
+            >
+                <Marker coordinate={hmCoordinates} image={require("./assets/Logo_Muenchen_Quadrat.png")} />
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    map: {
+        height: '100%',
+        width: '100%',
+    }
 });
